2bc7d89ab15264e1867b9466b3a7656f73bbea57
leafdev33 <125151094+leafdev33@users.noreply.github.com>
2023-04-16 14:48:43 -0400

    Create interaction_unit_test.py

---
+++ interaction_unit_test.py
@@ -0,0 +1,43 @@
+import unittest
+from Crypto.PublicKey import RSA
+from interaction import Interaction
+
+class TestInteraction(unittest.TestCase):
+
+    def test_create_sign_and_verify_interaction(self):
+        # Generate a public/private key pair
+        key = RSA.generate(2048)
+        public_key = key.publickey().exportKey()
+        private_key = key.exportKey()
+
+        # Create an interaction
+        interaction = Interaction("Sample interaction data", public_key)
+
+        # Sign the interaction
+        interaction.sign(private_key)
+
+        # Verify the interaction
+        self.assertTrue(interaction.verify())
+
+    def test_verify_invalid_signature(self):
+        # Generate a public/private key pair
+        key1 = RSA.generate(2048)
+        public_key1 = key1.publickey().exportKey()
+        private_key1 = key1.exportKey()
+
+        # Generate another public/private key pair
+        key2 = RSA.generate(2048)
+        public_key2 = key2.publickey().exportKey()
+        private_key2 = key2.exportKey()
+
+        # Create an interaction using public_key1
+        interaction = Interaction("Sample interaction data", public_key1)
+
+        # Sign the interaction with private_key2
+        interaction.sign(private_key2)
+
+        # Verify the interaction using public_key1 (expecting False since the wrong key was used to sign)
+        self.assertFalse(interaction.verify())
+
+if __name__ == '__main__':
+    unittest.main()
